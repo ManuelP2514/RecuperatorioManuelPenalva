ef7829f929530c6e1d42501bd2456fb5248c52a5
manue <manue@LAPTOP-TKTP7A3B>
2022-12-27 20:54:23 -0300

    Agregar archivos de proyecto.

---
+++ RecuperatorioManuelPenalva.Entidades/Relleno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecuperatorioManuelPenalva.Entidades
+{
+    
+        public enum Relleno
+        {
+
+            Blanco = 1,
+            Negro,
+            Rojo,
+            Azul,
+            Verde
+        }
+    
+}
---
+++ RecuperatorioManuelPenalva.Entidades/Trazo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecuperatorioManuelPenalva.Entidades
+{
+
+    public enum Trazo
+    {
+        Continuo = 1,
+        Rayas,
+        Puntos
+    }
+
+}
